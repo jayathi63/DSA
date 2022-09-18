bf157a84989734459bc88ce2e4216497ea00f351
jayathi63 <70006605+jayathi63@users.noreply.github.com>
2022-09-18 19:28:16 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -4,9 +4,9 @@ public class Sum {
     public static void main(String[] args)
     {
         System.out.print("Enter 'a' value : ");
-        int a = 1
+        int a = 1;
         System.out.print("\nEnter 'b' value : ");
-        int b = 3
+        int b = 3;
         System.out.println("\nSum of a,b = "+(a+b));
         input.close();
     }

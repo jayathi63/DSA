ec4777ae68228e3e1bcd72e355ce4c98cb2d970d
jayathi63 <70006605+jayathi63@users.noreply.github.com>
2022-09-18 22:57:27 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args)
     {
-        System.out.print("Enter 'a' value : ");
+        //System.out.print("Enter 'a' value : ");
         int a = 1;
-        System.out.print("\nEnter 'b' value : ");
+        //System.out.print("\nEnter 'b' value : ");
         int b = 3;
-        System.out.println("\nSum of a,b = "+(a+b));
+        if(a+b == 4)
+            return 0;
+        else
+            return -1
+        //System.out.println("\nSum of a,b = "+(a+b));
     }
 }
